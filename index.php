73a8b4aba0acc4ad82225ac9d232f143b3b90b46
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2025-10-13 12:16:06 +0100

    Update index.php
    testing a log in page

--- index.php
+++ index.php
@@ -1,10 +1,43 @@
-<!DOCTYPE html>
-<html>
-<body>
- 
 <?php
-echo "My first PHP script!";
+$error = "";
+$success = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if ($username === "user" && $password === "pass123") {
+        $success = "Login successful! Welcome, " . htmlspecialchars($username);
+    } else {
+        $error = "Invalid username or password.";
+    }
+}
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+
+    <?php if ($error): ?>
+        <p style="color:red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+
+    <?php if ($success): ?>
+        <p style="color:green;"><?php echo $success; ?></p>
+    <?php else: ?>
+        <form method="POST" action="">
+            <label>Username:</label><br>
+            <input type="text" name="username" required><br><br>
+
+            <label>Password:</label><br>
+            <input type="password" name="password" required><br><br>
+
+            <button type="submit">Login</button>
+        </form>
+    <?php endif; ?>
 </body>
 </html>
