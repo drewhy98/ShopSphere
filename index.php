cd2bec049ad45c1d31bd071670b50776c2ac0d1e
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2025-10-13 12:27:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,19 @@
-<?php
-$error = "";
-$success = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
-
-    if ($username === "user" && $password === "pass123") {
-        $success = "Login successful! Welcome, " . htmlspecialchars($username);
-    } else {
-        $error = "Invalid username or password.";
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Simple Login</title>
+    <title>Login Page</title>
 </head>
 <body>
     <h2>Login</h2>
+    <form method="POST" action="check_login.php">
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
 
-    <?php if ($error): ?>
-        <p style="color:red;"><?php echo $error; ?></p>
-    <?php endif; ?>
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
 
-    <?php if ($success): ?>
-        <p style="color:green;"><?php echo $success; ?></p>
-    <?php else: ?>
-        <form method="POST" action="">
-            <label>Username:</label><br>
-            <input type="text" name="username" required><br><br>
-
-            <label>Password:</label><br>
-            <input type="password" name="password" required><br><br>
-
-            <button type="submit">Login</button>
-        </form>
-    <?php endif; ?>
+        <button type="submit">Login</button>
+    </form>
 </body>
 </html>
